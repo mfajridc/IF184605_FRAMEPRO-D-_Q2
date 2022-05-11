7e90933741daa6e933aac8d48434f9140b2e830e
“mfajridc” <mfajridc@gmail.com>
2022-05-11 22:02:42 +0700

    Spring Boot Template

---
+++ src/test/java/com/example/paketlengkap/PaketlengkapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paketlengkap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaketlengkapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
